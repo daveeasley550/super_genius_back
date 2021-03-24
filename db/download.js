9876870720e4af84761a436c34cd2576cf6f7e45
ThatLivelyLlama <thatlivelyllama@gmail.com>
2021-03-24 13:05:29 -0600

    Aquired API Key, and Created Fetch request

---
+++ db/download.js
@@ -0,0 +1,40 @@
+require("./database.js")
+let fetch = require("node-fetch")
+let mongoose =require("mongoose")
+let artistModel = require("./artists/model")
+let songModel =require("./songs/model")
+let artistData = require("./artistAPIData.json")
+let songData = require("./songAPIData.json")
+let fs = require("fs").promises
+
+let artistURL = "http://ws.audioscrobbler.com/2.0/?method=chart.gettopartists&api_key=864ffe55118cd2944b873cafe1db1c40&format=json"
+let trackURL = "http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=864ffe55118cd2944b873cafe1db1c40&format=json"
+fetch(artistURL)
+    .then(res => res.json())
+    .then(data => fs.writeFile("./artistAPIData.json", JSON.stringify(data)))
+    .then(()=>{
+        artistModel
+            .deleteMany({})
+            .then(
+                artistModel.create(artistData).then(data=> {
+                    console.log(data.length)
+                    mongoose.disconnect()
+                })
+            )
+    })
+
+
+fetch(trackURL)
+    .then(res => res.json())
+    .then(data => fs.writeFile("./songAPIData.json", JSON.stringify(data)))
+    .then(()=>{
+        songModel
+            .deleteMany({})
+            .then(
+                songModel.create(songData).then(data=> {
+                    console.log(data.length)
+                    mongoose.disconnect()
+                })
+            )
+    })
+
