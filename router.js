0964f80d18337ff77cc82484cfd1ab224583fa95
Dave Easley <easley.david@gmail.com>
2021-03-24 14:21:09 -0400

    added 2nd route to external router

--- router.js
+++ router.js
@@ -2,6 +2,12 @@ let express = require('express')
 let artistRouter = require('./artists/router')
 
 let router = new express.Router()
-router.use('/artist', artistRouter)//need to make this for artist
+router.use('/artist', artistRouter)
+
+let express = require('express')
+let songRouter = require('./songs/router')
+
+let router = new express.Router()
+router.use('/songs', songRouter)
 
 module.exports = router
